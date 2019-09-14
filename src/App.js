f69abb2ee005acd109ccd1f02b5570640a44f2f4
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-09-14 09:44:54 +0000

    and so it Begins

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+     <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
